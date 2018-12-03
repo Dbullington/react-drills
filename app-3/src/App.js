36e38cdbf980befc5cf3d9fba33a81d6f0b5ebde
Daniel <danielrbullington@gmail.com>
2018-12-02 20:59:23 -0600

    finished apps 1-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filterString: "",
+      cars: [
+        "Ford",
+        "Chevy",
+        "Toyota",
+        "Honda",
+        "BMW",
+        "Honda",
+        "Mercades",
+        "Tesla"
+      ]
+    }
+  }
+
+  handleChange(filter) {
+    this.setState({filterString:filter})
+  }
+
   render() {
+    let carsToDisplay = this.state.cars.filter((element, index) => {
+      return element.includes(this.state.filterString);
+    }).map ((element, index) => {
+      return <h1 key = {index} > {element}</h1>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange = { (e) => this.handleChange(e.target.value)} type="text" />
+        {carsToDisplay}
       </div>
     );
   }
